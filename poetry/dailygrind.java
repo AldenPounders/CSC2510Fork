ce1ab7cc8bcbe33eceafd8ee97f4ed84e1112bd3
AldenPounders <apounders42@tntech.edu>
2024-02-02 17:13:18 -0600

    Update dailygrind.java

--- poetry/dailygrind.java
+++ poetry/dailygrind.java
@@ -15,6 +15,12 @@ while (away_the_hours) {
 	if (its_time_to_go_home) {
          break;
     }
+    try {
+    Thread.sleep(60000);
+ } catch (InterruptedException e) {
+ 	// ignore
+ )
+
 }
 
     }
