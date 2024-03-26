95722393693c1d17547d8773fd8ebdef75708d69
Alden Pounders <aldenpounders@tntech.edu>
2024-03-26 16:56:19 +0000

    lab09 finished

--- utils.sh
+++ utils.sh
@@ -6,3 +6,7 @@ find /usr | head
 grep "model name" /proc/cpuinfo
 grep -c -v "sudo" /etc/group
 grep -n  "sudo" /etc/group
+sort food
+sort -r food
+sort -k 2 food
+sort -k 4,4 -k 3,3 -r -n food > calo
